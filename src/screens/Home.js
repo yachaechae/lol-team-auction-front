218906fd24f09de178b55117fd2a37dcf4c26153
yachaechae <yachaechae0o0@gmail.com>
2022-05-18 19:21:34 +0900

    트위치 로그인

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Home() {
+
+    const [twitchName, settwitchName] = useState()
+    const urlCode = new URLSearchParams(document.location.hash.slice(1)).get("access_token")
+    axios.get('https://id.twitch.tv/oauth2/userinfo',
+    {
+      headers : {
+        Authorization : `Bearer ${urlCode}`
+      }
+    }).then((result)=>{
+        settwitchName(result.data.preferred_username)
+    })
+    
+    console.log(twitchName)
+
+
     return (
         <div className="home">
                 <h1 className="title">롤 팀 경매장</h1>
@@ -18,6 +34,7 @@ export default function Home() {
                        선수 등록
                     </Link>
                 </div>
+                <a href="https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=nwt477t3f1o4vh15qf3pdqyle90c4d&redirect_uri=http://localhost:3000&scope=channel%3Amanage%3Apolls+channel%3Aread%3Apolls&state=c3ab8aa609ea11e793ae92361f002671">테스트</a>        
         </div>
     )
 }
