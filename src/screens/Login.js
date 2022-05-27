ee8a196c26f7d9dfec0b1d112505f788920562ed
yachaechae <yachaechae0o0@gmail.com>
2022-05-27 20:07:47 +0900

    로그인페이지 분리

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,30 +1,49 @@
 import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { useRecoilState } from 'recoil'
-import { loginInfo } from './State'
+import { loginInfoAtom } from './State'
 
 export default function Login() {
     
-    const [token, setToken] = useRecoilState(loginInfo)
+    const [loginInfo, setLoginInfo] = useRecoilState(loginInfoAtom)
+    const navigator = useNavigate()
 
     useEffect(() => {
-        const urlCode = new URLSearchParams(document.location.hash.slice(1)).get("access_token")
+        const token = new URLSearchParams(document.location.hash.slice(1)).get("access_token")
         
-        if(urlCode !== null) {
-            setToken(urlCode)
+        if(!!token) {
+            setLoginInfo({
+                token: token
+            })
         }
-
-
     }, [])
+
+    useEffect(() => {
+        if(!!loginInfo.token) {
+            navigator('/')
+        }
+    }, [loginInfo])
+    
     const login = () =>{
-        window.open('https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=nwt477t3f1o4vh15qf3pdqyle90c4d&redirect_uri=http://localhost:3000&scope=channel%3Amanage%3Apolls+channel%3Aread%3Apolls&state=c3ab8aa609ea11e793ae92361f002671','_self')
+        window.open('https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=nwt477t3f1o4vh15qf3pdqyle90c4d&redirect_uri=http://localhost:3000/login&scope=channel%3Amanage%3Apolls+channel%3Aread%3Apolls&state=c3ab8aa609ea11e793ae92361f002671','_self')
 
     }
 
   return (
     <div>
-        <button className="btn btn-border" onClick={() =>{login()}}>
-            로그인
-        </button>
+        <a class="login-btn" onClick={() =>{login()}}>
+            <span class="login-btn__outline login-btn__outline--tall">
+                <span class="login-btn__line login-btn__line--tall"></span>
+                <span class="login-btn__line login-btn__line--flat"></span>
+            </span>
+            <span class="login-btn__outline login-btn__outline--flat">
+                <span class="login-btn__line login-btn__line--tall"></span>
+                <span class="login-btn__line login-btn__line--flat"></span>
+            </span>
+            <span class="login-btn__solid"></span>
+            <span class="login-btn__text">Login</span>
+        </a>
+
     </div>
     
   )
